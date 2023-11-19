f33dab4eaa7d52ba07cd29e72da3f2f2676e4295
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2023-11-19 21:44:29 +0100

    systemy baz danych lista 1

---
+++ sem3/Systemy_baz_danych/lab/lista1.sql
@@ -0,0 +1,20 @@
+--create schema world authorization pwr;
+/*
+create table country (
+	ID integer primary key,
+	Name varchar(255),
+	Capital varchar(255),
+	Population integer,
+	Created_on timestamp,
+	HDI real
+);
+*/
+--alter table country add column GDP integer;
+
+--alter table country drop column Created_on;
+
+--truncate table country;
+
+--drop table country;
+
+--select * from country;
