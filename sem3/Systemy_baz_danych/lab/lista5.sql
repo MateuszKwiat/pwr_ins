4c95f5df7e41b7e88fadd08afb37db6c57aad7fe
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2023-12-15 23:57:31 +0100

    list 5 ex 1, 4, 6

--- sem3/Systemy_baz_danych/lab/lista5.sql
+++ sem3/Systemy_baz_danych/lab/lista5.sql
@@ -1,5 +1,7 @@
 -- 1
-
+select distinct on (doc->'government'->>'GovernmentForm') 
+doc->'government'->>'GovernmentForm'
+from countryinfo
 
 -- 2
 select count(doc->'geography'->>'Continent'), doc->'geography'->>'Continent'
@@ -15,11 +17,19 @@ where doc->>'IndepYear' != 'null'
 order by cast(doc->>'IndepYear' as integer) desc
 
 -- 4
-
+select Language, count(Language) filter (where IsOfficial = 'T')
+from countryLanguage
+group by Language
+order by count(Language) desc
 
 -- 5
 
 
 -- 6
-
+select doc->>'Name', doc->>'GNP', doc->'demographics'->>'LifeExpectancy'
+from countryinfo
+where doc->>'GNP' != '0' anddoc->'demographics'->>'LifeExpectancy' != 'null'
+order by cast(doc->>'GNP' as float) desc,
+cast(doc->'demographics'->>'LifeExpectancy' as float) desc
+limit 20
 
