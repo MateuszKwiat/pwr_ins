1cca50d0bd9d0ecdb10821982e30ce1ffe1450fd
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2023-12-15 22:44:47 +0100

    list 5 ex 3

--- sem3/Systemy_baz_danych/lab/lista5.sql
+++ sem3/Systemy_baz_danych/lab/lista5.sql
@@ -8,9 +8,11 @@ group by doc->'geography'->>'Continent'
 order by count(doc->'geography'->>'Continent') desc
 limit 1
 
-
 -- 3
-
+select doc->>'Name', doc->>'IndepYear' 
+from countryinfo
+where doc->>'IndepYear' != 'null'
+order by cast(doc->>'IndepYear' as integer) desc
 
 -- 4
 
