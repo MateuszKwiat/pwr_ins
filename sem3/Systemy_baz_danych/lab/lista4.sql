cc72899ba37d3ee05bac20c8fdbd7db6448b98bd
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2023-12-15 20:50:52 +0100

    list 4 ex 6

--- sem3/Systemy_baz_danych/lab/lista4.sql
+++ sem3/Systemy_baz_danych/lab/lista4.sql
@@ -1,15 +1,25 @@
+-- 2
 select doc->'Name', doc->'GNP' from countryinfo
 	where (doc->>'GNP')::float =
 	(select max((doc->>'GNP')::float)
 		from countryinfo)
 
-
+-- 3
 select max((doc->>'GNP')::float), min((doc->>'GNP')::float), 
 avg((doc->>'GNP')::float)
 from countryinfo group by (doc->'geography'->'Continent')::text
 
-
+-- 4
 select city.name, countryinfo.doc->'geography'->>'Continent' from city
 left join countryinfo on countryinfo.doc->>'_id'=city.CountryCode
 where countryinfo.doc->'geography'->>'Continent'='North America'
 order by city.name
+
+-- 5
+
+
+-- 6
+
+
+-- 7
+
