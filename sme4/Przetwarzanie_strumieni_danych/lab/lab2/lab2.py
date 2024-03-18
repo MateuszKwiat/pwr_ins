91659daa6c946fb0249d2d41ea79318c3645c703
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2024-03-18 14:10:50 +0100

    przetwarzanie strumieni danych lab2, exercise 2

--- sme4/Przetwarzanie_strumieni_danych/lab/lab2/lab2.py
+++ sme4/Przetwarzanie_strumieni_danych/lab/lab2/lab2.py
@@ -1,9 +1,16 @@
 import cv2
 import matplotlib.pyplot as plt
 
-#[ZAD_1]
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+
+# [ZAD_1]
 img = cv2.imread('image.png')
-plt.imshow(img)
-plt.show()
+axes[0].imshow(img)
 
 cv2.imwrite('doggo.png', img)
+
+# [ZAD_2]
+img = cv2.cvtColor(cv2.imread('image.png'), cv2.COLOR_BGR2GRAY)
+axes[1].hist(img)
+
+plt.show()
