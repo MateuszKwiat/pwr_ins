9f1111f4898b0f56b982947cd638c5b91c14c34a
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2024-04-04 18:17:40 +0200

    przetwarzanie strumieni danych lab3, minor code improvements

--- sme4/Przetwarzanie_strumieni_danych/lab/lab3/lab3.py
+++ sme4/Przetwarzanie_strumieni_danych/lab/lab3/lab3.py
@@ -2,14 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-
 t = np.linspace(0, 20, 1000)
 
-sine_signal = np.sin(2 * np.pi * t)
+sine_signal = np.sin(40 * np.pi * t)
 square_signal = signal.square(2 * np.pi * t)
 sawtooth_signal = signal.sawtooth(2 * np.pi * t)
 chirp_signal = signal.chirp(t, f0=1, f1=0.5, t1=3)
-superposition_signal = np.sin(2 * np.pi * t) + np.cos(2 * np.pi * t)
+superposition_signal = np.sin(20 * np.pi * t) + np.cos(40 * np.pi * t)
 unit_impulse_signal = signal.unit_impulse(len(t))
 
 # [ZAD_1]
@@ -51,19 +50,11 @@ plt.show()
 
 # [ZAD_2]
 def rxx(n, r, sig):
-    sum = 0
-
-    for x1, x2 in zip(sig, sig[r:]):
-        sum += x1 * x2
-
-    return sum / (n - r)
+    return sum([x1 * x2 for x1, x2 in zip(sig, sig[r:])]) / (n - r)
 
 def psd(sig):
-    result = []
+    result = [rxx(len(sig), r, sig[:len(sig) - r]) for r in range(len(sig))]
 
-    for r in range(len(sig)):
-        result.append(rxx(len(sig), r, sig[:len(sig) - r]))
-    
     return np.linspace(0, 0.5, len(sig)//2), np.fft.fft(result)[:len(sig)//2].real
 
 # [ZAD_3]
@@ -73,7 +64,7 @@ signals = [np.sin(2 * np.pi * t), signal.square(2 * np.pi * t),
            signal.sawtooth(2 * np.pi * t), signal.chirp(t, f0=1, f1=0.5, t1=3), 
            np.sin(2 * np.pi * t) + np.cos(2 * np.pi * t), signal.unit_impulse(len(t))]
 
-i = 2
+i = 0
 
 axes[0].plot(*signal.periodogram(signals[i]), color='red')
 axes[1].plot(*signal.welch(signals[i]), color='orange')
