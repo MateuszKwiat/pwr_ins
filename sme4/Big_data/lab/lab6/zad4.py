6b08414883d3c8bdc216e85285cb25e3e6df0b25
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2024-05-05 13:05:44 +0200

    big data lab6, exercise4, tests addition

--- sme4/Big_data/lab/lab6/zad4.py
+++ sme4/Big_data/lab/lab6/zad4.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.stats import f_oneway
 from sklearn.datasets import load_wine
 
 wine = load_wine()
@@ -8,21 +8,24 @@ X = wine.data
 Y = wine.target
 
 U, s, VT = np.linalg.svd(X)
-k = 3
+k = 1
 
-reduced_data = np.dot(U[:, :k], np.diag(s[:k]))
+reduced_data = U[:, :k] * s[:k]
+
+while any(f_oneway(X, np.dot(reduced_data, VT[:k])).pvalue < 0.05):
+    k += 1
+    reduced_data = U[:, :k] * s[:k]
+
+print(f'final k: {k}')
 
 fig, ax = plt.subplots(figsize=(14, 7))
 
-ax = plt.axes(projection='3d')
-
 for label in np.unique(Y):
-    ax.scatter3D(reduced_data[Y==label, 0], reduced_data[Y==label, 1], reduced_data[Y==label, 2], label=wine.target_names[label], alpha=0.75)
+    ax.scatter(reduced_data[Y==label, 0], reduced_data[Y==label, 1], label=wine.target_names[label], alpha=0.75)
 
 ax.set_title('reduction of dimensions with SVD on wine dataset')
 ax.set_xlabel('first dimension')
 ax.set_ylabel('second dimension')
-ax.set_zlabel('third dimension')
 
 ax.legend(loc='upper right')
 
