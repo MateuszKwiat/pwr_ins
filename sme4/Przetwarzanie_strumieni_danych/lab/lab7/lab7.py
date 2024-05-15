32e7a716280129642e5af4e2de756a0838afbf5a
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2024-05-15 15:38:55 +0200

    przetwarzanie strumieni danych lab7, exercise2

--- sme4/Przetwarzanie_strumieni_danych/lab/lab7/lab7.py
+++ sme4/Przetwarzanie_strumieni_danych/lab/lab7/lab7.py
@@ -2,6 +2,8 @@ import pywt
 import numpy as np
 import matplotlib.pyplot as plt
 
+from matplotlib.widgets import Slider
+
 # [ZAD_1]
 make_wavelet = lambda name, isContinuous=False: pywt.ContinuousWavelet(name).wavefun(10) if isContinuous else pywt.Wavelet(name).wavefun(10)
 
@@ -14,5 +16,33 @@ fig.tight_layout()
 for wavelet, params, ax in zip(wavelets, wavelet_params, axes.ravel()):
     ax.plot(*(wavelet[::-1] if params[-1] == True else (wavelet[-1], wavelet[1])), color='orange')
     ax.set_title(params[0] + ' wavelet')
+
+plt.show()
+
+# [ZAD_2]
+def plot_wavelet(wavelet):
+    ax.plot(wavelet[-1], wavelet[1], color='orange')
+    ax.set_title('Daubechies wavelet')
+
+def update(*args):
+    wavelet = db_wavelet(ax_version_slider.val, ax_level_slider.val)
+    
+    ax.clear()
+    plot_wavelet(wavelet)
+    fig.canvas.draw_idle()
+
+fig, ax = plt.subplots()
+
+db_wavelet = lambda version=2, level=2: pywt.Wavelet(f'db{version}').wavefun(level)
+
+ax_version = fig.add_axes([0.12, 0.01, 0.55, 0.025])
+ax_version_slider = Slider(ax=ax_version, label='version', valmin=2, valmax=35, valinit=2, valstep=1, orientation='horizontal')
+
+ax_level = fig.add_axes([0.12, 0.05, 0.55, 0.025])
+ax_level_slider = Slider(ax=ax_level, label='level', valmin=2, valmax=10, valinit=2, orientation='horizontal')
+
+plot_wavelet(db_wavelet())
+ax_version_slider.on_changed(update)
+ax_level_slider.on_changed(update)
 
 plt.show()
