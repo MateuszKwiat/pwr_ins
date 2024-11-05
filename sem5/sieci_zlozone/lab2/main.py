daf8acb26bc7971c144723bf360b87046ee17ccf
MateuszKwiat <mateusz.kwiat316@gmail.com>
2024-11-05 13:43:07 +0100

    complex networks, lab2, ex1

---
+++ sem5/sieci_zlozone/lab2/main.py
@@ -0,0 +1,14 @@
+import networkx as nx
+from networkx.linalg.graphmatrix import incidence_matrix
+
+fh = open("congress.edgelist", "rb")
+G = nx.read_edgelist(fh)
+fh.close()
+nx.set_edge_attributes(G, {(u, v): {'capacity' : 1} for u, v, ed in G.edges.data()})
+
+# zad1
+A = nx.incidence_matrix(G)
+B = nx.adjacency_matrix(G)
+print(A)
+print('-'*40)
+print(B)
